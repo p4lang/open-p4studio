3c6a9f3eada7a85d19d88e2ed35223aba6842e0b
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2025-01-18 17:50:37 +0000

    Additions to post-build script
    Signed-off-by: Andy Fingerhut <andy_fingerhut@alum.wustl.edu>

--- show-build-logs.sh
+++ show-build-logs.sh
@@ -4,22 +4,36 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # Basic system info
+set -x
 uname -a
 nproc
 cat /proc/meminfo
+gcc --version
+set +x
+for f in p4studio/logs/*
+do
+    set -x
+    wc $f
+    set +x
+done
 
+set +x
 echo "------------------------------------------------------------"
 echo "Occurrences of 'killed process' in dmesg logs are a likely sign"
 echo "that a process was killed by the kernel due to the system running"
 echo "our of memory, leading to unpredictable build failures."
 echo "------------------------------------------------------------"
+set -x
 sudo dmesg -T | egrep -i 'killed process'
 
+set +x
 echo "------------------------------------------------------------"
 echo "Full output of command: dmesg -T"
 echo "------------------------------------------------------------"
+set -x
 sudo dmesg -T
 
+set +x
 for f in p4studio/logs/*
 do
     echo "------------------------------------------------------------"
