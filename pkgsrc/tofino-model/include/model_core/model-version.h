0b89fa46d9f5e972b41f68d063b9b3737a9b3588
fruffy <fruffy@nyu.edu>
2025-02-03 17:23:08 -0500

    Cmake cleanups.
    Signed-off-by: fruffy <fruffy@nyu.edu>

--- pkgsrc/tofino-model/include/model_core/model-version.h
+++ pkgsrc/tofino-model/include/model_core/model-version.h
@@ -1,8 +1,8 @@
-#define MODEL_TAG "39dbad7b-dirty"
+#define MODEL_TAG "83a9fc11-dirty"
 #ifndef MODEL_TAG
 #define MODEL_TAG "000"
 #endif
 // If MODEL_BRANCH is HEAD, it indicates that git repo was in
 // detached head state.
 #define MODEL_BRANCH prathima/include_bf_model
-#define MODEL_COMMIT_ID 39dbad7ba8588772f7c7f4eebe7cdd3063a56155
+#define MODEL_COMMIT_ID 83a9fc11d23ff8bf7054ad7f6cc0bd03cd31f96a
