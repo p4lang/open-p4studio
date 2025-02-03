651dc6b9ba56b3f51945a90ee95ad1eb2cda3608
fruffy <fruffy@nyu.edu>
2025-02-03 16:58:49 -0500

    Model version?!
    Signed-off-by: fruffy <fruffy@nyu.edu>

--- pkgsrc/tofino-model/include/model_core/model-version.h
+++ pkgsrc/tofino-model/include/model_core/model-version.h
@@ -1,8 +1,8 @@
-#define MODEL_TAG "b6f6fca6-dirty"
+#define MODEL_TAG "39dbad7b-dirty"
 #ifndef MODEL_TAG
 #define MODEL_TAG "000"
 #endif
 // If MODEL_BRANCH is HEAD, it indicates that git repo was in
 // detached head state.
 #define MODEL_BRANCH prathima/include_bf_model
-#define MODEL_COMMIT_ID b6f6fca623bf612d61224fd10e433567cc0df6b0
+#define MODEL_COMMIT_ID 39dbad7ba8588772f7c7f4eebe7cdd3063a56155
